622a0df98e1df53d5c9d38ebb8c4c10ac06a52c1
rasitha11 <rasithasen@gmail.com>
2020-09-08 12:23:18 +0530

    Treatment details bean commit

--- ICU/src/com/bean/TreatmentDetails.java
+++ ICU/src/com/bean/TreatmentDetails.java
@@ -11,5 +11,53 @@ public class TreatmentDetails {
 	private double fintake;
 	private double foutput;
 	
+	public int getTreatmentDetailsID() {
+		return treatmentDetailsID;
+	}
+	public void setTreatmentDetailsID(int treatmentDetailsID) {
+		this.treatmentDetailsID = treatmentDetailsID;
+	}
+	public int getPatientID() {
+		return patientID;
+	}
+	public void setPatientID(int patientID) {
+		this.patientID = patientID;
+	}
+	public int getDoctorID() {
+		return doctorID;
+	}
+	public void setDoctorID(int doctorID) {
+		this.doctorID = doctorID;
+	}
+	public double getbP() {
+		return bP;
+	}
+	public void setbP(double bP) {
+		this.bP = bP;
+	}
+	public int gethR() {
+		return hR;
+	}
+	public void sethR(int hR) {
+		this.hR = hR;
+	}
+	public double getsPO2() {
+		return sPO2;
+	}
+	public void setsPO2(double sPO2) {
+		this.sPO2 = sPO2;
+	}
+	public double getFintake() {
+		return fintake;
+	}
+	public void setFintake(double fintake) {
+		this.fintake = fintake;
+	}
+	public double getFoutput() {
+		return foutput;
+	}
+	public void setFoutput(double foutput) {
+		this.foutput = foutput;
+	}
 
 }
