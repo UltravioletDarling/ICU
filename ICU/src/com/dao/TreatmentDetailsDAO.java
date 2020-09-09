5677b4a5a159c808397aacf95bade76979fb8239
rasitha11 <rasithasen@gmail.com>
2020-09-09 09:35:25 +0530

    Display all treatment details full Commit

--- ICU/src/com/dao/TreatmentDetailsDAO.java
+++ ICU/src/com/dao/TreatmentDetailsDAO.java
@@ -2,8 +2,10 @@ package com.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.sql.Statement;
+import java.util.ArrayList;
 import com.bean.TreatmentDetails;
 import com.util.DBConnectionRAZ;
 
@@ -12,16 +14,16 @@ public class TreatmentDetailsDAO implements ITreatmentDetailsDAO{
 	public void addTreatmentDetails(TreatmentDetails treatmentDetails) throws ClassNotFoundException
     {
 		 
-   	 Connection con = null;
+		Connection con = null;
         PreparedStatement preparedStatement = null;
     
         
         try {
             con = DBConnectionRAZ.getConnection();
-            String query = "insert into treatmentdetails" + "(treatmentDetailsID, patientID, doctorID, bP, hR, sPO2, fintake, foutput) values "
+            String sql = "insert into treatmentdetails" + "(treatmentDetailsID, patientID, doctorID, bP, hR, sPO2, fintake, foutput) values "
             + " (?,?,?,?,?,?,?,?)";
             
-            preparedStatement = con.prepareStatement(query);
+            preparedStatement = con.prepareStatement(sql);
             preparedStatement.setInt(1, treatmentDetails.getTreatmentDetailsID());
             preparedStatement.setString(2, treatmentDetails.getPatientID());
             preparedStatement.setString(3, treatmentDetails.getDoctorID());
@@ -42,6 +44,70 @@ public class TreatmentDetailsDAO implements ITreatmentDetailsDAO{
            e.printStackTrace();
         }       
     }
+	
+	
+	public TreatmentDetails getTreatmentDetailsByID(String treatmentdetailsID) {
 
+		return actionOnTreatmentDetails(treatmentdetailsID).get(0);
+	}
+	
+	
 
+	
+	public ArrayList<TreatmentDetails> getTreatmentDetails() {
+		
+		return actionOnTreatmentDetails(null);
+	}
+	
+	
+	
+	private ArrayList<TreatmentDetails> actionOnTreatmentDetails(String treatmentdetailsID) {
+		
+			//declare variables
+			ArrayList<TreatmentDetails> treatmentDetailsList = new ArrayList<TreatmentDetails>();
+			String sql;
+			Connection connection = null;		
+			Statement statement = null;
+			//execution		
+			try {
+			//establish connection	
+			connection = DBConnectionRAZ.getConnection();
+			//statement 
+			statement = connection.createStatement();
+				//check what action to take with if statement 
+			if(treatmentdetailsID!= null && !treatmentdetailsID.isEmpty()) {
+				 sql = "select * from treatmentdetails where treatmentDetailsID = " + treatmentdetailsID ;
+			}else {
+				sql ="select * from treatmentdetails";
+			}
+			//execute query
+			ResultSet resultSet = statement.executeQuery(sql);
+			
+			//sort query result in to array list
+			while(resultSet.next()) {
+				TreatmentDetails treatmentDetails = new TreatmentDetails();
+				treatmentDetails.setTreatmentDetailsID(resultSet.getInt(1));
+				treatmentDetails.setPatientID(resultSet.getString(2));
+				treatmentDetails.setDoctorID(resultSet.getString(3));
+				treatmentDetails.setbP(resultSet.getInt(4));
+				treatmentDetails.sethR(resultSet.getInt(5));
+				treatmentDetails.setsPO2(resultSet.getDouble(6));
+				treatmentDetails.setFintake(resultSet.getDouble(7));
+				treatmentDetails.setFoutput(resultSet.getDouble(8));
+				treatmentDetailsList.add(treatmentDetails);
+			}
+			//close statement and connection
+			if (statement != null) statement.close();
+			if (connection != null) connection.close();
+			
+	
+		}catch(Exception e){
+			
+		}
+		return treatmentDetailsList;
+	}
+	
+	
 }
+
+
